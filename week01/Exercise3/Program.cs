364099ed98aef5720345b7d08b49f951922e9346
MuhireB1 <muhirebienvenue@gmail.com>
2026-01-13 17:05:11 +0200

    2

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Write("Enter magic number: ");
+        int magic_number = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter your guess: ");
+        int guess = int.Parse(Console.ReadLine());
+
+        while (guess != magic_number)
+        {
+            if (guess < magic_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magic_number)
+            {
+                Console.WriteLine("Lower");
+            }
+        }
+
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
     }
 }
