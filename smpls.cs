c1a37a8369c162a1a25a557a2f111b36f5fa387f
MuhireB1 <muhirebienvenue@gmail.com>
2026-01-13 14:04:24 +0200

    first commit

---
+++ smpls.cs
@@ -0,0 +1,10 @@
+int x = 8;
+int t = 15;
+if (x < t)
+{
+    Console.WriteLine($"{x} is less than {t}.");
+}
+else
+{
+    Console.WriteLine($"{t} is greater than {x}");
+}
--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        String first_name = Console.ReadLine();
+
+        Console.Write("What is your second name? ");
+        String second_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {second_name}, {first_name} {second_name}.");
+
     }
 }
+
