96092f5300e54268bf96b45e3b41b5a94ef9f08a
MuhireB1 <muhirebienvenue@gmail.com>
2026-01-19 16:10:07 +0200

    1

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Instance 1
+        Fraction fr1 = new Fraction();
+        Fraction fr2 = new Fraction(6);
+        Fraction fr3 = new Fraction(6, 7);
+
+
         Console.WriteLine("Hello World! This is the Fractions Project.");
     }
 }
+
