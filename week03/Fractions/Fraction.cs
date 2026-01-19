029a83ef5d5f113cd89b04c4289ea98be276ef9a
MuhireB1 <muhirebienvenue@gmail.com>
2026-01-19 16:12:42 +0200

    1

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -3,53 +3,49 @@ public class Fraction
     private int _top;
     private int _bottom;
 
-    public class Fraction
+    /// Constructors
+    public Fraction()
     {
-        private int _top;
-        private int _bottom;
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+    }
 
-        /// Constructors
-        public Fraction()
-        {
-            _top = 1;
-            _bottom = 1;
-        }
-        public Fraction(int wholeNumber)
-        {
-            _top = wholeNumber;
-        }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
 
-        public Fraction(int top, int bottom)
-        {
-            _top = top;
-            _bottom = bottom;
-        }
+    //Getters and Setters
+    public int GetTop()
+    {
+        return _top;
+    }
 
-        public int GetTop()
-        {
-            return _top;
-        }
+    public int SetTop(int top)
+    {
+        _top = top;
+    }
 
-        public int SetTop(int top)
-        {
-            _top = top;
-        }
+    public int getBottom()
+    {
+        return _bottom;
+    }
+    public int setBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
 
-        public int getBottom()
-        {
-            return _bottom;
-        }
-        public int setBottom(int bottom)
-        {
-            _bottom = bottom;
-        }
+    //Methods
+    public string GetFractionString()
+    {
+    }
+    public double GetDecimalValue()
+    {
 
-        public string GetFractionString()
-        {
-        }
-        public double GetDecimalValue()
-        {
-
-        }
     }
 }
